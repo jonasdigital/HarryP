97e07a2189d2d0aa512b96a1cbe7c43ca206e526
Oliver <o.berggren@hotmail.com>
2021-01-20 10:50:09 +0100

    ObjectPooler Scripts

---
+++ Assets/Scripts/MushroomSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomSpawner : MonoBehaviour
+{
+    ObjectPooler objectPooler;
+
+    private void Start()
+    {
+        objectPooler = ObjectPooler.Instance;
+    }
+    void FixedUpdate()
+    {
+        objectPooler.spawnFromPool("Mushroom", transform.position, Quaternion.identity);
+    }
+}
