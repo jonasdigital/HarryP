ece4aecc003cead91139cca2f7de24a66dbd0504
jonasdigital <digitalbostad@gmail.com>
2021-01-20 15:11:42 +0100

    StartScene+Music

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadScene : MonoBehaviour
+{
+
+
+
+    public void Scene1()
+    {
+        SceneManager.LoadScene("Jonas");
+    }
+
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+}
