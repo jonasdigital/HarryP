f120ab9f45baa2027de131281e7ac8a7f485f86a
DannyFowlTheRanger <63231876+DannyFowlTheRanger@users.noreply.github.com>
2021-01-21 10:07:52 +0100

    Added Basket holder

---
+++ Assets/Scripts/BasketHolder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketHolder : MonoBehaviour
+{
+    public GameObject Mushroom;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Grabbable")
+        {
+            Mushroom.transform.parent = other.gameObject.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == Mushroom)
+        {
+            Mushroom.transform.parent = null;
+        }
+    }
+
+}
