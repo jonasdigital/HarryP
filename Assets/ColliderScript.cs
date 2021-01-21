86b66a7d9778d9f2ae783508ff63cc5facd48836
Oliver <o.berggren@hotmail.com>
2021-01-21 14:52:25 +0100

    ColliderScript

---
+++ Assets/ColliderScript.cs
@@ -0,0 +1,36 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderScript : MonoBehaviour
+{
+    private bool cube1;
+    private bool cube2;
+    private bool cube3;
+    public MushroomSpawner svampar;
+    public void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.CompareTag("Grabbable"))
+        {
+            cube1 = true;
+        }
+
+        if (collision.gameObject.CompareTag("WitchWater"))
+        {
+            cube2 = true;
+        }
+
+        if(collision.gameObject.CompareTag("Stick"))
+        {
+            cube3 = true;
+        }
+        
+        if (cube1 && cube2 && cube3)
+        {
+            //Debug.Log("Enemy and Ground collision");
+            svampar.spawnMushroom = true;
+        }
+    }
+}
--- Assets/Scripts/MushroomSpawner.cs
+++ Assets/Scripts/MushroomSpawner.cs
@@ -1,26 +1,30 @@
-﻿using System.Collections;
+﻿using NUnit.Framework.Constraints;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MushroomSpawner : MonoBehaviour
 {
+    public bool spawnMushroom;
+    
     ObjectPooler objectPooler;
-
     private void Start()
     {
     objectPooler = ObjectPooler.Instance;
+        spawnMushroom = false;
     }
-
-    //private void OnCollisionEnter(Collision collisionInfo)
-    //{
-        //if (collisionInfo.collider.CompareTag("Grabbable"))
-        //{
-            //objectPooler = ObjectPooler.Instance;
-        //}
-    //}
+     
     void FixedUpdate()
-    {
-        objectPooler.spawnFromPool("Mushroom", transform.position, Quaternion.identity);
+    {        
+        if (spawnMushroom == true)
+        {           
+            objectPooler.spawnFromPool("Mushroom", transform.position, Quaternion.identity);
+        }
+        else
+        {
+            Debug.Log("Inga svampar");
+        }     
     }
 }
 
